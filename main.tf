fd2361c43a2be6f864f74114c08c29ead60b8f93
David Angarita <josdeivi90@hotmail.com>
2025-10-26 12:12:06 -0500

    added storage account and ouputs files

--- main.tf
+++ main.tf
@@ -8,11 +8,24 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "44e49970-899b-4abc-aaf8-322b3e531fbe"
+  subscription_id = var.subscription_id
   features {}
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = "eastus"
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  public_network_access_enabled = false
+
+  tags = {
+    environment = "testing"
+  }
 }
---
+++ output.tf
@@ -0,0 +1,12 @@
+output "storage_account_connection_string" {
+    value = azurerm_storage_account.storage_account.primary_connection_string
+    sensitive = true
+}
+
+output "storage_account_name" {
+    value = azurerm_storage_account.storage_account.name
+}
+
+output "storage_account_primary_blob_endpoint" {
+    value = azurerm_storage_account.storage_account.primary_blob_endpoint
+} 
