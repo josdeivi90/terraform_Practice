841eb6232723797b8e51796ccd8a6e412eeeebde
David Angarita <josdeivi90@hotmail.com>
2025-10-26 16:18:05 -0500

    fmt

--- main.tf
+++ main.tf
@@ -1,28 +1,14 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.40.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  subscription_id = var.subscription_id
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = "eastus"
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                          = var.storage_account_name
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
   public_network_access_enabled = false
 
   tags = {
