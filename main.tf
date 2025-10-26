d40b0811e2c58bd8b41b76923f592eb2384eb2e8
David Angarita <josdeivi90@hotmail.com>
2025-10-26 11:31:33 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "44e49970-899b-4abc-aaf8-322b3e531fbe"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-test-peke"
+  location = "eastus"
+}
